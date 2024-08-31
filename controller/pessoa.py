007241a46e167657c628f7cc7be0418d89ee6a41
Carlos Eduardo <carlos.aeduardo@hotmail.com>
2024-08-31 15:05:47 -0300

    Padrão Strategy

--- controller/pessoa.py
+++ controller/pessoa.py
@@ -1,8 +1,41 @@
 from data.pessoa import PessoaDAOimp
 from model.pessoa import Passageiro, Piloto
 
+class PilotoStrategy:
+    
+    def get_all_pilotos(self):
+        db = PessoaDAOimp()
+        todos_pilotos = db.get_all_pilotos()
+        db.close()
+        return [Piloto(p[0], p[1], p[2], p[3]) for p in todos_pilotos]
+    
+    def get_piloto_with_id(self, id):
+        db = PessoaDAOimp()
+        piloto = db.get_piloto_with_id(id)
+        db.close()
+        return Piloto(piloto[0], piloto[1], piloto[2], piloto[3])
+    
 
-class PessoaController:
+class PassageiroStrategy:
+    def get_all_passageiros(self):
+        db = PessoaDAOimp()
+        todos_passageiros = db.get_all_passageiros()
+        db.close()
+        return [Passageiro(p[0], p[1], p[2]) for p in todos_passageiros]
+    
+    def get_passageiro_with_id(self, id):
+        db = PessoaDAOimp()
+        passageiro = db.get_passageiro_with_id(id)
+        db.close()
+        return Passageiro(passageiro[0], passageiro[1], passageiro[2], passageiro[3])
+    
+    def update_reserva_passageiro(self, id_passageiro, reserva):
+        db = PessoaDAOimp()
+        db.update_reserva_passageiro(int(id_passageiro), reserva.id)
+        db.close()
+        return True
+    
+class PessoaController(PilotoStrategy, PassageiroStrategy):
 
 
     def add(self, nome, idade, tipo, numero_carteira):
@@ -17,35 +50,12 @@ class PessoaController:
         db.close()
         return True
 
-    def get_all_pilotos(self):
-        db = PessoaDAOimp()
-        todos_pilotos = db.get_all_pilotos()
-        db.close()
-        return [Piloto(p[0], p[1], p[2], p[3]) for p in todos_pilotos]
-    
-    def get_all_passageiros(self):
-        db = PessoaDAOimp()
-        todos_passageiros = db.get_all_passageiros()
-        db.close()
-        return [Passageiro(p[0], p[1], p[2]) for p in todos_passageiros]
-    
     def get_all(self):
         db = PessoaDAOimp()
         todas_pessoas = db.get_all_pessoas()
         return [Passageiro(p[0], p[1], p[2]) if p[-1] == 'Passageiro' else Piloto(p[0], p[1], p[2], p[3]) for p in todas_pessoas]
 
-    def get_passageiro_with_id(self, id):
-        db = PessoaDAOimp()
-        passageiro = db.get_passageiro_with_id(id)
-        db.close()
-        return Passageiro(passageiro[0], passageiro[1], passageiro[2], passageiro[3])
-    
-    def get_piloto_with_id(self, id):
-        db = PessoaDAOimp()
-        piloto = db.get_piloto_with_id(id)
-        db.close()
-        return Piloto(piloto[0], piloto[1], piloto[2], piloto[3])
-    
+
     def remove(self, id, tipo):
         
         db = PessoaDAOimp()
@@ -56,10 +66,6 @@ class PessoaController:
         db.close()
         return True
     
-    def update_reserva_passageiro(self, id_passageiro, reserva):
-        db = PessoaDAOimp()
-        db.update_reserva_passageiro(int(id_passageiro), reserva.id)
-        db.close()
-        return True
+    
         
 
